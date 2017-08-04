8ef1e2ce1c6ba43b6dc99df552ed72599e7533b8
52lions06 <mckelvey.william@yahoo.com>
2017-08-04 11:43:53 -0400

    added folders -- components,actions,reducers,store created initialState

---
+++ client/src/reducers/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const initialState = {
+  cheeses: []
+};
+
+const cheeses = (state=initialState, action) => {
+  if (aciton.type === action.______ ){
+    return Object.assign({}, state, {cheeses: })
+  }
+  else {
+    return state 
+  }
+}
